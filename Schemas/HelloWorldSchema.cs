c3863688a205be071688b37f1ee8c5ebe1bc4b12
Lin <Leo.Lin@chubb.com>
2018-11-20 18:56:11 +0900

    first commit

---
+++ Schemas/HelloWorldSchema.cs
@@ -0,0 +1,9 @@
+using GraphQL.Types;
+
+public class HelloWorldSchema : Schema
+{
+    public HelloWorldSchema(HelloWorldQuery query)
+    {
+        Query = query;
+    }
+}
---
+++ Types/HelloWorldQuery.cs
@@ -0,0 +1,15 @@
+using GraphQL.Types;
+public class HelloWorldQuery: ObjectGraphType
+{
+    public HelloWorldQuery()
+    {
+        Field<StringGraphType>(
+            name: "hello",
+            resolve: context => "world"
+        );
+        Field<StringGraphType>(
+            name: "howdy",
+            resolve: context => "universe"
+        );
+    }
+}
